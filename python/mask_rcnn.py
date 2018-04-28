210aafaea54e96c7c0084ffc5f06a4780d9dd33f
mattmcd <matt@matt-mcdonnell.com>
2018-04-28 14:32:51 +0100

    Working demo running locally on amazonlinux docker image.

--- python/mask_rcnn.py
+++ python/mask_rcnn.py
@@ -1,18 +1,35 @@
 import os
-import sys
-import random
-import math
-import numpy as np
-# import skimage.io
+import skimage.io
+
+# Import Mask RCNN
+import mrcnn.model as modellib
+import mrcnn.coco as coco
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
 
-# Import Mask RCNN
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
-import mrcnn.coco as coco
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, 'data', 'model', "mask_rcnn_coco.h5")
+
+
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
 
 
 class InferenceConfig(coco.CocoConfig):
@@ -22,8 +39,27 @@ class InferenceConfig(coco.CocoConfig):
     IMAGES_PER_GPU = 1
 
 
+def init():
+    config = InferenceConfig()
+
+    # Create model object in inference mode.# Creat
+    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+    # Load weights trained on MS-COCO
+    model.load_weights(COCO_MODEL_PATH, by_name=True)
+    return model
+
+
+def demo(image='cows_800x600.jpg'):
+    # Create model and run prediction on a pic of cows
+    model = init()
+    IMAGE_DIR = os.path.join(ROOT_DIR, 'data', 'images')
+    image = skimage.io.imread(os.path.join(IMAGE_DIR, image))
+    results = model.detect([image], verbose=1)
+    r = results[0]
+    print([class_names[i] for i in r['class_ids']])
+    return r
 
 
 if __name__ == '__main__':
-    config = InferenceConfig()
-    config.display()
+    model = init()
